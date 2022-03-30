e3836749d0518ec78c715a4853afb7007cfac908
Ashlee <ashleeryan@outlook.com>
2022-03-30 14:00:28 +0100

    test

---
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -0,0 +1,22 @@
+package com.example.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+	@GetMapping("/hello")
+	public String get() {
+		return "Hello Word";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
